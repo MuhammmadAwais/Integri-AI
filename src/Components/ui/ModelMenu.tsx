cf2467d443c7ed09421f542bb0b8e0d35846aa08
Awais Latif <97788862+MuhammmadAwais@users.noreply.github.com>
2026-01-08 19:36:34 +0500

    Commit vercel fixes for images not loading

--- src/Components/ui/ModelMenu.tsx
+++ src/Components/ui/ModelMenu.tsx
@@ -164,7 +164,7 @@ const ModelMenu: React.FC<ModelMenuProps> = ({
                     )}
                   >
                     <div>
-                      <img src={isDark ? `/${m.dark_theme_logo}` : `/${m.light_theme_logo}`} alt="logo" className="w-6 h-6 shrink-0"  />
+                      <img src={isDark ? `${m.dark_theme_logo}` : `${m.light_theme_logo}`} alt="logo" className="w-6 h-6 shrink-0"  />
                       <div className="font-medium">{m.label || m.id}</div>
                       <div className="text-[10px] opacity-60 capitalize flex items-center gap-1.5">
                         {m.alt_provider || m.provider}
